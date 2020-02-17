0e2f933bf9b3bf7d068a72706c3632110187b649
ushasree2429 <32006838+ushasree2429@users.noreply.github.com>
2020-02-17 00:21:39 -0800

    Delete the pads.sql

--- sql/the pads.sql
+++
@@ -1,2 +0,0 @@
-select name+'('+left(occupation,1)+')' from occupations order by name;
-select "There are a total of",count(*) t,lower(occupation)+'s.' from occupations group by occupation order by t, occupation ;
