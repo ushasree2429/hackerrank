d41daa19e2e9be95f977f1af728c26294389f64d
ushasree2429 <32006838+ushasree2429@users.noreply.github.com>
2020-02-17 00:19:52 -0800

    Create new companies.sql

---
+++ sql/advanced select/new companies.sql
@@ -0,0 +1,1 @@
+select c.company_code, founder, count(distinct(lead_manager_code)),count(distinct(senior_manager_code)),count(distinct(manager_code)),count(distinct(employee_code)) from company c join employee e on c.company_code=e.company_code group by c.company_code,founder order by c.company_code;
