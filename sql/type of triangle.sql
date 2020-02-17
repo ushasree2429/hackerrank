1bc46fc25da37b7233f5eb49893efdc3e16fe9e8
ushasree2429 <32006838+ushasree2429@users.noreply.github.com>
2020-02-17 00:22:33 -0800

    Delete type of triangle.sql

--- sql/type of triangle.sql
+++
@@ -1,8 +0,0 @@
-select  
-(case 
-when a+b >c and a=b and b=c then "Equilateral"
-when a+b >c and a=b or b=c or a=c then " Isosceles"
-when a!=b and a!=c and b!=c and a+b>c then "Scalene"
-else "Not A Triangle"
-end )t
-from triangles;
